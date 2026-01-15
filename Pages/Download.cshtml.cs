5374fd877b31da7991eed2bf8656305b5a8ac9b7
Primica <artbrun@icloud.com>
2026-01-15 11:46:49 +0100

    Add Download page and update navigation links for CSBNorm

---
+++ Pages/Download.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace csbnorme_net.Pages
+{
+    public class DownloadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
