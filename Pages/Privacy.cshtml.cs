0954920de7302c8faa87792d766290fc0396e6ee
Primica <artbrun@icloud.com>
2026-01-15 11:27:07 +0100

    Refactor code structure for improved readability and maintainability

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace csbnorme_net.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
---
+++ Pages/Rules.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace csbnorme_net.Pages
+{
+    public class RulesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
